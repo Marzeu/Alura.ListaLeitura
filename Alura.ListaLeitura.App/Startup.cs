f6c19b3b141d602f87b9a52a283b1aa05b47a4df
Marzeu <marzahell@hotmail.com>
2021-04-05 16:00:11 -0300

    refatorando actions

--- Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura.App/Startup.cs
@@ -8,13 +8,13 @@ namespace Alura.ListaLeitura.App
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRouting();
+        {            
             services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseDeveloperExceptionPage();
             app.UseMvcWithDefaultRoute();
         }
     }
